95e0279c29cd2dfe3189d7dcba3218a2af87d648
David Hughes <hello@davidhughes.co>
2018-03-23 14:57:15 +0000

    New styles for home

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,9 +1,21 @@
 class ResponsesController < InheritedResources::Base
 
+  def create
+    @response = Response.new(response_params)
+
+    respond_to do |format|
+      if @response.save
+        format.html { redirect_to response_path(@response) }
+      else
+        format.html { render :new }
+        format.json { render json: @response.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
     def response_params
       params.require(:response).permit(:test_id, :name, :answer)
     end
 end
-
